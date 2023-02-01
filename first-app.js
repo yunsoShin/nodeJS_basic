457802ca73dd7e8faf2b51a79ad6fb03697ccad8
yunsoo1 <shin4895@gmail.com>
2023-02-01 20:33:07 +0900

    writeFileSync

---
+++ first-app.js
@@ -0,0 +1,23 @@
+/*
+const fs = require('fs');
+
+fs.writeFileSync('안녕.txt','hello from Nod.js') //파일 생성하는 메서드 '인수는 파일이름,파일내용,혹은 옵션 호출이 반복되면 덮어씌워짐'
+
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+*/
