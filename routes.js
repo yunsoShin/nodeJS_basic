97a9a53d197d4b3cca74b1f95e17c262e0fa11f9
yunsoo1 <shin4895@gmail.com>
2023-02-04 21:15:53 +0900

    nodemon
    pakage setup

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ const requestHandler = (req, res) => {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send!</button></form></body>'
     );
     res.write('</html>');
     return res.end();
