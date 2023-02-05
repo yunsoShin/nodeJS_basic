e567d1cbca8095499c554684e6f3efa0b4232b48
yunsoo1 <shin4895@gmail.com>
2023-02-05 21:01:15 +0900

    module

--- app.js
+++ app.js
@@ -4,14 +4,25 @@ const express = require('express');
 
 const app = express();
 
+app.use('/add-product',(req, res, next) => {
+    console.log('addproduct'); 
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+
+    
+});
+app.use('/product',(req,res,next)=>{
+    res.redirect('/');
+})
+
+
 app.use((req, res, next) => {
     console.log('In the middleware!');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    res.send('<h1>Hello from Express!!</h1>');
 });
 
 const server = http.createServer(app);
