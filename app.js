bafa560f659490f93ca4dc1d8b5da280eeda060b
yunsoo1 <shin4895@gmail.com>
2023-02-06 22:51:11 +0900

    router , 404 error page

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/add-product',(req, res, next) => {
-    console.log('addproduct'); 
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-    
-});
-app.use('/product',(req,res,next)=>{
-    res.redirect('/');
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.use('/',(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!!</h1>');
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
---
+++ programs.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    let 짝수=0;
+    var answer = [];
+    if(num_list[1]%2==0){
+        짝수=+1
+    }
+    console.log(짝수);
+    return answer;
+}
+
+solution([1,2,3,4,5])
+
+
